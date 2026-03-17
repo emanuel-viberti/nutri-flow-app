cd166496a1479f489d0cf3399b24639101e9dcd4
emanuel-viberti <lic.emanuelviberti@gmail.com>
2026-03-17 17:09:23 -0300

    Update calculator.py

--- modules/calculator.py
+++ modules/calculator.py
@@ -1,16 +1,21 @@
+# modules/calculator.py
+
 def calcular_metricas(sexo, talla, peso_act, edad, pi_real, af_val, p_pro, p_cho, p_lip):
-    # Cálculo de TMB (Mifflin-St Jeor)
+    # 1. TMB (Mifflin-St Jeor)
     tmb = (10 * pi_real) + (6.25 * talla) - (5 * edad) + (5 if sexo == "Masculino" else -161)
     
-    # Cálculo de GET
+    # 2. GET (Gasto Energético Total)
     get = tmb * af_val
     
-    # Cálculo de IMC
+    # 3. IMC
     imc = peso_act / ((talla/100)**2)
     
-    # Gramos de macros
-    obj_p = (get * p_pro / 400)
-    obj_c = (get * p_cho / 400)
-    obj_l = (get * p_lip / 900)
+    # 4. Cálculo de Gramos Objetivo
+    # Multiplicamos GET por el % y dividimos por (100 * kcal por gramo)
+    # Proteínas: 4 kcal/g | Carbohidratos: 4 kcal/g | Lípidos: 9 kcal/g
+    
+    obj_p = (get * (p_pro / 100)) / 4
+    obj_c = (get * (p_cho / 100)) / 4
+    obj_l = (get * (p_lip / 100)) / 9
     
     return get, imc, obj_p, obj_c, obj_l
