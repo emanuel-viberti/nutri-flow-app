77e55e517f078e932409e56345f61f46fcbb7dc1
emanuel-viberti <lic.emanuelviberti@gmail.com>
2026-03-17 16:57:35 -0300

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,14 @@
+# Configuración global y constantes
+LIMITES_SEMANALES = {
+    "Pollo al horno con calabaza": 3,
+    "Yogur con granola y {frutilla}": 4,
+    "Wok de tofu y arroz integral": 2,
+    "Tostadas con {palta} y huevo": 4,
+    "Peceto con ensalada de {choclo}": 3
+}
+
+paises = {
+    "Argentina 🇦🇷": {"zapallito": "Zapallito", "palta": "Palta", "choclo": "Choclo", "frutilla": "Frutilla", "carne": "Corte Magro (Nalga/Peceto)", "legumbre": "Lentejones"},
+    "México 🇲🇽": {"zapallito": "Calabacita", "palta": "Aguacate", "choclo": "Elote", "frutilla": "Fresa", "carne": "Bistec", "legumbre": "Frijoles"},
+    "España 🇪🇸": {"zapallito": "Calabacín", "palta": "Aguacate", "choclo": "Maíz", "frutilla": "Fresa", "carne": "Ternera", "legumbre": "Alubias"}
+}
